7275470719d722d3ae519340384423027fd4a6f1
David <David@DESKTOP-VJMTNVA>
2018-10-15 10:37:42 +0100

    Controllers for the Views

---
+++ LMS_Ui/Controllers/ModulesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LMS_Ui.Controllers
+{
+    public class ModulesController : Controller
+    {
+        // GET: Modules
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
