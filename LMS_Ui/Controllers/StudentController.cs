abcbe13ecfcede6201acb2434d145e23bfbeb8be
David <David@DESKTOP-VJMTNVA>
2018-10-17 13:01:57 +0100

    worked on the student content view

---
+++ LMS_Ui/Controllers/StudentController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LMS_Ui.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ViewLessonPlan()
+        {
+            return View();
+        }
+
+        public ActionResult ViewContent()
+        {
+            return View();
+        }
+    }
+}
